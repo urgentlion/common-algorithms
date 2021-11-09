4358871e54a1bd6ddcb903bf6c5382970f6da1ff
urgentlion <urgentlioninfo@gmail.com>
2021-11-09 15:35:33 -0800

    ...

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const mergeSort = arr => {
-    if(arr.length === 1) {
-        return arr;
-    }
-
-    const center = arr.length / 2;
-
-    const left = arr.slice(0, center);
-    const right = arr.slice(center);
-
-    return merge(mergeSort(left), mergeSort(right));
-}
-
-const merge = (left, right) => {
-    const results = [];
-
-    while(left.length && right.length) {
-        if(left[0] < right[0]) {
-             results.push(left.shift());
+const fizzBuzz = n => {
+    for(let i = 1; i <= n; i++) {
+        if(i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if(i % 3 === 0) {
+            console.log('fizz');
+        } else if(i % 5 === 0) {
+            console.log('buzz');
         } else {
-             results.push(right.shift());
+            console.log(i);
         }
     }
-    return [...results, ...left, ...right];
 };
 
-const result = merge([-33, 2], [0, -6]);
-console.log(result);
-const result2 = mergeSort([7, 3, -12, 5]);
-console.log(result2);
+fizzBuzz(30);
