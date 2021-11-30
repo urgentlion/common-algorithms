cbf68b05287c1cd313f5e42e2246fae56d4ce844
urgentlion <urgentlioninfo@gmail.com>
2021-11-30 13:51:22 -0800

    ...

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const chunk = (arr, size) => {
-    const array = [];
-
-    for(let el of arr) {
-        const last = array[array.length - 1];
-
-        if(!last || last.length === size) {
-            array.push([el]);
-        } else {
-            last.push(el);
-        }
+const mergeSort = arr => {
+    if(arr.length === 1) {
+        return arr;
     }
-    return array;
+
+    const divide = arr.length / 2;
+
+    const left = arr.slice(0, divide);
+    const right = arr.slice(divide);
+
+    //merge helper function
+    return merge(mergeSort(left), mergeSort(right));
 };
 
-const result = chunk([1,2,3,4], 2);
-console.log(result); 
+const merge = (left, right) => {
+
+    mergedArray = [];
+
+    while(left.length && right.length) {
+        if(left[0] < right[0]) {
+            mergedArray.push(left.shift());
+        } else {
+            mergedArray.push(right.shift());
+        }
+    }
+    return [ ...mergedArray, ...left, ...right];
+};
+
+const result = mergeSort([10, 0 , 97, -6, 5]);
+console.log(result);
+
+const result2 = merge([3,4], [-2,13]);
+console.log(result2);
